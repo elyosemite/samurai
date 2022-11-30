40a17c38ecb4a1977f5b72884c64a1ce1f6f1eed
Yuri Melo <yuri.melo@jurify.com.br>
2022-11-30 10:11:58 -0300

    add new python code

--- PIX/person.py
+++ PIX/person.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 class Person:
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
+
     def __init__(self, nome, idade, comendo=False, falando=False):
         self.nome = nome
         self.idade = idade
@@ -8,21 +12,49 @@ class Person:
         variavel_valida_dentro_do_init = True
         print(variavel_valida_dentro_do_init)
     
-    def comer(self):
+    def comer(self, fruta):
         if self.comendo:
-            print(f'{self.nome} já está comendo')
+            print(f'{self.nome} já está comendo {fruta}')
             return
 
-        print(f'{self.comendo} está comendo');
+        if self.falando:
+            print(f'{self.nome} não pode comer falando')
+            return
+
+        print(f'{self.nome} está comendo');
         self.comendo = True
     
-    def falar(self):
-        print(f'Falando ...')
+    def falar(self, mensagem):
+        if self.comendo:
+            print(f'{self.nome} não pode falar comendo.')
+            return
+
+        if self.falando:
+            print(f'{self.nome} já está falando.')
+            return
+
+        print(f'{self.nome} está falando ... {mensagem}')
+        self.falando =True
+
+    def parar_falar(self):
+        if not self.falando:
+            print(f'{self.nome} não está falando')
+            return
+        
+        print(f'{self.nome} parou de falar')
+        self.falando = False
 
     def parar_comer(self):
         if not self.comendo:
             print(f'{self.nome} Não está comendo')
             return
         
-        print(f'{self.nome} parou ded comer.')
+        print(f'{self.nome} parou de comer.')
         self.comendo = False
+    
+    def obter_ano_nascimento(self):
+        return self.ano_atual - self.idade
+
+    @classmethod
+    def por_ano_nascimento(cls):
+        pass
--- main.py
+++ main.py
@@ -6,5 +6,8 @@ from PIX import (
 p1 = Person('Yuri', 24)
 user = User()
 
-p1.falar()
-user.PegarInfo()
+p1.comer('maçã')
+p1.falar('Poo')
+p1.parar_comer()
+p1.falar('POO')
+p1.comer('Alimento')
