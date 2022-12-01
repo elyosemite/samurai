78fac450dcd4f2512748261ffbcaae404cf54462
Yuri Melo <yuri.melo@jurify.com.br>
2022-12-01 10:16:05 -0300

    create getters and setters

---
+++ models/product.py
@@ -0,0 +1,30 @@
+from msilib.schema import Property
+
+class Product(object):
+    """description of class"""
+    def __init__(self, name, price) -> None:
+        self.name = name
+        self.price = price
+
+    def discount(self, percent):
+        self.price = self.price - (self.price * (percent / 100))
+
+    # Getter
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def name(self):
+        return self._name
+
+    # Setter
+    @price.setter
+    def price(self, value):
+        if isinstance(value, str):
+            current_value = float(value.replace('R$', ''))
+        self._price = value
+
+    @name.setter
+    def name(self, value):
+        self._name = value.title()
