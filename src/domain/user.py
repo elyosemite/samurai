e273de342ede9e11a22b5f3224af1b9d80b9f807
Yuri Melo <yurifullstack@gmail.com>
2025-08-28 16:38:06 -0300

    Add domain model

--- src/domain/user.py
+++ src/domain/user.py
@@ -1,8 +1,12 @@
-class User:
-    def __init__(self, name: str, age: int, city: str, occupation: str, company: str, investiment: float):
+from src.domain.entity import Entity
+
+class User(Entity):
+    def __init__(self, name: str, age: int, city: str, occupation: str, company: str, investiment: float, entity_id=None):
+        super().__init__(entity_id)
         self.name = name
         self.age = age
         self.city = city
         self.occupation = occupation
         self.company = company
         self.investiment = investiment
+        print(f"User created with ID: {self.id}")
